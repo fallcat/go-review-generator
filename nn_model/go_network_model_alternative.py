4ea7a144885394ea5f34ee83e6f9494d4b12f2fd
Xiaoyue <sunxy30@wharton.upenn.edu>
2020-12-03 01:03:09 -0500

    fixed getting the wrong board features

--- nn_model/go_network_model_alternative.py
+++ nn_model/go_network_model_alternative.py
@@ -48,13 +48,40 @@ train_comment = file_obj.readlines()
 file_obj = open(dataset_dir + '/train.choices.pkl', 'rb')
 train_choice = pickle.load(file_obj)
 
-# simple nlp for now
-nlp_sm = en_core_web_sm.load()
-train_comment_vec = []
-for i in range(len(train_comment)):
-    train_comment_vec.append(nlp_sm(train_comment[i]).vector)
+def init_vocab(vocab_file, cutoff):
+  vocab = {}
+  i = 2
+  with open(vocab_file, encoding='utf8') as f:
+    for line in f:
+      line = line.strip().split(' ')
+      token = line[0]
+      count = int(line[1])
+      if count > cutoff:
+        vocab[token] = i
+        i += 1
+  return vocab
 
-def prepare_data(board_obj, comment_obj, comment_vec_obj, choice_obj):
+train_vocab = init_vocab(dataset_dir + '/vocab.32000', 0)
+
+Max_Length = 100
+comment_num = []
+comment_length = []
+for i in range(len(train_comment)):
+    line = train_comment[i].strip().split(' ')
+    convert = [train_vocab.get(token, 1) for token in line]
+    if len(convert) > Max_Length:
+        convert = convert[:Max_Length]
+        c_len = len(convert)
+    else:
+        c_len = len(convert)
+        convert += [0] * (Max_Length - c_len)
+    if c_len==0:
+        raise ValueError("Comment length is 0")
+    comment_num.append(convert)
+    comment_length.append(c_len)
+
+
+def prepare_data(board_obj, comment_obj, comment_vec_obj, comment_len_obj,choice_obj):
     """
     initial process for the data
     """
@@ -63,39 +90,50 @@ def prepare_data(board_obj, comment_obj, comment_vec_obj, choice_obj):
     X_board = []
     X_comment = []
     X_comment_vec = []
+    X_comment_len = []
     y = []
 
     players = {'b': 0, 'w':1}
 
     for i in range(len(choice_obj['answers'])):
-        pos_idx = choice_obj['answers'][i]
+        answer_index = choice_obj['answers'][i]
         choice_list = choice_obj['choice_indices'][i]
-        b = boards[i]
-        board_features = [b[0], b[1], players[b[2]]]
+        pos_index = choice_list[answer_index]  # select positive from the choice list 
+
+        b_pos = boards[pos_index]
+        board_features = [b_pos[0], b_pos[1], players[b_pos[2]]]
         X_move.append(board_features)
-        X_board.append(b[3])
-        X_comment.append(comment_obj[choice_list[pos_idx]])
-        X_comment_vec.append(comment_vec_obj[choice_list[pos_idx]])
+        X_board.append(b_pos[3])
+        X_comment.append(comment_obj[pos_index])
+        X_comment_vec.append(comment_vec_obj[pos_index])
+        X_comment_len.append(comment_len_obj[pos_index])
         y.append(1)
 
         # now for negative comment 
-        if pos_idx == 0:
-            neg_idx = 1
+        if answer_index == 0:
+            neg_idx = choice_list[1]
         else:
-            neg_idx = 0
-        X_move.append(board_features)
-        X_board.append(b[3])
-        X_comment.append(comment_obj[choice_list[neg_idx]])
-        X_comment_vec.append(comment_vec_obj[choice_list[neg_idx]])
+            neg_idx = choice_list[0]
+        b_neg = boards[neg_idx]
+        board_features_neg = [b_neg[0], b_neg[1], players[b_neg[2]]]
+        X_move.append(board_features_neg)
+        X_board.append(b_neg[3])
+        X_comment.append(comment_obj[neg_idx])
+        X_comment_vec.append(comment_vec_obj[neg_idx])
+        X_comment_len.append(comment_len_obj[neg_idx])
         y.append(0)
 
-    return X_board, X_move, X_comment, X_comment_vec, y
+    return X_board, X_move, X_comment, X_comment_vec, X_comment_len, y
 
-X_board, X_move, X_comment, X_comment_vec, y = prepare_data(train_board, train_comment, train_comment_vec, train_choice)
+X_board, X_move, X_comment, X_comment_vec, X_comment_len, y = prepare_data(train_board, train_comment, comment_num, comment_length, train_choice)
 
 
-final_train = data_utils.TensorDataset(torch.Tensor(np.array(X_board)), torch.Tensor(np.array(X_move)), torch.tensor(np.array(X_comment_vec), dtype = torch.long), torch.tensor(np.array(y), dtype=torch.long))
-train_loader = data_utils.DataLoader(final_train, batch_size = 32, shuffle=True)
+final_train = data_utils.TensorDataset(torch.Tensor(np.array(X_board)), \
+     torch.Tensor(np.array(X_move)), \
+     torch.tensor(np.array(X_comment_vec), dtype = torch.long),\
+     torch.tensor(np.array(X_comment_len), dtype=torch.long),
+     torch.tensor(np.array(y), dtype=torch.long))
+train_loader = data_utils.DataLoader(final_train, batch_size = 64, shuffle=True)
 
 
 def train_model(net, criterion, optimizer, n_epochs):
@@ -111,12 +149,12 @@ def train_model(net, criterion, optimizer, n_epochs):
         for i, data in enumerate(train_loader, 0):
             # get the inputs
 
-            x1, x2, x3, labels = data
+            x1, x2, x3, x4, labels = data
 
             # zero the parameter gradients
             optimizer.zero_grad()
             # forward + backward + optimize
-            outputs = net(x1, x2, x3)
+            outputs = net(x1, x2, x3, x4)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
@@ -163,32 +201,34 @@ def model_eval(net):
     print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
     return test_accuracy
 
+vocab_size = len(train_vocab)+2
 
 class MyModel(nn.Module):
-    def __init__(self,  hidden_dim = 64):
+    def __init__(self, dimension=64):
         super(MyModel, self).__init__()        
         # layers for board
         self.features1 = nn.Sequential(
             nn.Conv2d(1, 3, 3, 1, 1),
             nn.MaxPool2d(2),
-            nn.ReLU(),)
+            nn.ReLU())
         # layers for move & player
         self.features2 = nn.Sequential(
             nn.Conv2d(1, 3, 3, 1, 1),
-            nn.ReLU(),)
-
+            nn.ReLU())
 
         self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(284, 32)
+        self.fc1 = nn.Linear(253, 32)
         self.classifier = nn.Linear(32, 2)
 
-        self.embeddings = nn.Embedding(0, 100)
-        self.lstm = nn.LSTM(100, hidden_dim, batch_first=True)
-        self.linear = nn.Linear(hidden_dim, 32)
-        self.dropout = nn.Dropout(0.2)
+        # layer for comments
+        self.embedding = nn.Embedding(vocab_size, 100)
+        self.dimension = dimension
+        self.lstm = nn.LSTM(input_size=100, hidden_size=dimension, num_layers=1,
+                            batch_first=True, bidirectional=True)
+        self.drop = nn.Dropout(p=0.2)
+        self.fc = nn.Linear(2*dimension, 1)
         
-        
-    def forward(self, x_board, x_move, text):
+    def forward(self, x_board, x_move, text, text_len):
         # reshape x_board, x_move
         x1 = x_board.unsqueeze(1)
         x2 = x_move.unsqueeze(1)
@@ -197,23 +237,30 @@ class MyModel(nn.Module):
         x1 = self.features1(x1)
         x2 = self.features2(x2)
 
-        x3 = self.embeddings(text)
-        x3 = self.dropout(x3)
-        lstm_out, (ht, ct) = self.lstm(x3)
-        x3 = self.linear(ht[-1])
+        text_emb = self.embedding(text)
+        #print(text_len)
+        packed_input = pack_padded_sequence(text_emb, text_len, batch_first=True, enforce_sorted=False)
+        packed_output, _ = self.lstm(packed_input)
+        output, _ = pad_packed_sequence(packed_output, batch_first=True)
 
+        out_forward = output[range(len(output)), text_len - 1, :self.dimension]
+        out_reverse = output[:, 0, self.dimension:]
+        out_reduced = torch.cat((out_forward, out_reverse), 1)
+        text_fea = self.drop(out_reduced)
+
+        x3 = self.fc(text_fea)
         #text_fea = torch.squeeze(text_fea, 1)
         #text_out = torch.sigmoid(text_fea)
 
         x1 = x1.view(x1.size(0), -1)
         x2 = x2.view(x2.size(0), -1)
-
-        x3 = x3.reshape(x3.size(0), -1)
+        x3 = x3.view(x3.size(0), -1)
         
         x = torch.cat((x1, x2, x3), dim=1)
         x = self.relu(self.fc1(x))
-        x = self.classifier(x)
+        x = torch.sigmoid(x)
         return x
+   
 
 net_cnnlstm_test = MyModel()
 
@@ -221,5 +268,5 @@ net_cnnlstm_test = MyModel()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(net_cnnlstm_test.parameters())
 
-model, losses, accuracies = train_model(net_cnnlstm_test, criterion, optimizer, n_epochs = 20)
+model, losses, accuracies = train_model(net_cnnlstm_test, criterion, optimizer, n_epochs = 5)
 
