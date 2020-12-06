9e6fadce6346cb49ce05ba91455f770e0824dec5
Xiaoyue <sunxy30@wharton.upenn.edu>
2020-12-06 01:38:23 -0500

    added eval, reformat data processing

--- nn_model/go_network_model_alternative.py
+++ nn_model/go_network_model_alternative.py
@@ -37,16 +37,19 @@ import nltk
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 # os.chdir('D:/GoReview')
-dataset_dir = "data_splits_final"
+dataset_dir = "D:/GoReview/data_splits_final"
 
-file_obj = open(dataset_dir + '/train.pkl', 'rb')
-train_board = pickle.load(file_obj)
+def raw_file_loader(dataset_dir, fboard, fcomment, fchoice):
+    file_obj = open(dataset_dir + fboard, 'rb')
+    raw_board = pickle.load(file_obj)
 
-file_obj = open(dataset_dir + '/train_comments.tok.32000.txt', 'r', encoding='utf-8')
-train_comment = file_obj.readlines()
+    file_obj = open(dataset_dir + fcomment, 'r', encoding='utf-8')
+    raw_comment = file_obj.readlines()
 
-file_obj = open(dataset_dir + '/train.choices.pkl', 'rb')
-train_choice = pickle.load(file_obj)
+    file_obj = open(dataset_dir + fchoice, 'rb')
+    raw_choice = pickle.load(file_obj)
+
+    return raw_board, raw_comment, raw_choice
 
 def init_vocab(vocab_file, cutoff):
   vocab = {}
@@ -63,22 +66,24 @@ def init_vocab(vocab_file, cutoff):
 
 train_vocab = init_vocab(dataset_dir + '/vocab.32000', 0)
 
-Max_Length = 100
-comment_num = []
-comment_length = []
-for i in range(len(train_comment)):
-    line = train_comment[i].strip().split(' ')
-    convert = [train_vocab.get(token, 1) for token in line]
-    if len(convert) > Max_Length:
-        convert = convert[:Max_Length]
-        c_len = len(convert)
-    else:
-        c_len = len(convert)
-        convert += [0] * (Max_Length - c_len)
-    if c_len==0:
-        raise ValueError("Comment length is 0")
-    comment_num.append(convert)
-    comment_length.append(c_len)
+def create_comment_vec(raw_comment, vocabu):
+    Max_Length = 100
+    c_num = []
+    c_length = []
+    for i in range(len(raw_comment)):
+        line = raw_comment[i].strip().split(' ')
+        convert = [vocabu.get(token, 1) for token in line]
+        if len(convert) > Max_Length:
+            convert = convert[:Max_Length]
+            c_len = len(convert)
+        else:
+            c_len = len(convert)
+            convert += [0] * (Max_Length - c_len)
+        if c_len==0:
+            raise ValueError("Comment length is 0")
+        c_num.append(convert)
+        c_length.append(c_len)
+    return c_num, c_length
 
 
 def prepare_data(board_obj, comment_obj, comment_vec_obj, comment_len_obj, choice_obj):
@@ -125,31 +130,60 @@ def prepare_data(board_obj, comment_obj, comment_vec_obj, comment_len_obj, choic
 
     return X_board, X_move, X_comment, X_comment_vec, X_comment_len, y
 
+
+'''Load training set'''
+train_board, train_comment, train_choice = raw_file_loader(dataset_dir, '/train.pkl', '/train_comments.tok.32000.txt', '/train.choices.pkl')
+comment_num, comment_length = create_comment_vec(train_comment, train_vocab)
 X_board, X_move, X_comment, X_comment_vec, X_comment_len, y = prepare_data(train_board, train_comment, comment_num, comment_length, train_choice)
 
-
-final_train = data_utils.TensorDataset(torch.Tensor(np.array(X_board)), \
-     torch.Tensor(np.array(X_move)), \
-     torch.tensor(np.array(X_comment_vec), dtype = torch.long),\
-     torch.tensor(np.array(X_comment_len), dtype=torch.long),
-     torch.tensor(np.array(y), dtype=torch.float32))
+final_train = data_utils.TensorDataset(torch.Tensor(np.array(X_board[0:200000])), \
+     torch.Tensor(np.array(X_move[0:200000])), \
+     torch.tensor(np.array(X_comment_vec[0:200000]), dtype = torch.long),\
+     torch.tensor(np.array(X_comment_len[0:200000]), dtype=torch.long),
+     torch.tensor(np.array(y[0:200000]), dtype=torch.float32))
 train_loader = data_utils.DataLoader(final_train, batch_size = 64, shuffle=True)
+
+test_board, test_comment, test_choice = raw_file_loader(dataset_dir, '/test.pkl', '/test_comments.tok.32000.txt', '/test.choices.pkl')
+comment_num_test, comment_length_test = create_comment_vec(test_comment, train_vocab)
+X_board_test, X_move_test, X_comment_test, X_comment_vec_test, X_comment_len_test, y_test = prepare_data(test_board, test_comment, comment_num_test, comment_length_test, test_choice)
+
+final_test = data_utils.TensorDataset(torch.Tensor(np.array(X_board_test)), \
+     torch.Tensor(np.array(X_move_test)), \
+     torch.tensor(np.array(X_comment_vec_test), dtype = torch.long),\
+     torch.tensor(np.array(X_comment_len_test), dtype=torch.long),
+     torch.tensor(np.array(y_test), dtype=torch.float32))
+test_loader = data_utils.DataLoader(final_test, batch_size = 64, shuffle=True)
+
+def model_eval(net):
+    """ evaluate test set 
+    """
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            var1,var2,var3,var4, labels = data
+            outputs = net(var1,var2,var3,var4)
+            labels = labels.unsqueeze(1)
+            pred = (outputs >= 0.5).int()
+            total += labels.size(0)
+            correct += (pred == labels).sum().item()
+
+    test_accuracy = 100 * correct / total
+    print('Accuracy of the network on the test sets: %d %%' % (100 * correct / total))
+    return test_accuracy
 
 
 def train_model(net, criterion, optimizer, n_epochs):
     losses = []
     accuracies = []
-    minibatch_accuracies = []
+    test_accuracies = []
 
-    corrected = 0
-    total = 0
+    net.train()
     for epoch in tqdm.tqdm(range(n_epochs)):  # loop over the dataset multiple times
         since = time.time()
         running_loss = 0
         running_correct = 0.0
         for i, data in enumerate(train_loader, 0):
-            # get the inputs
-            running_loss_bat = 0
 
             x1, x2, x3, x4, labels = data
 
@@ -163,48 +197,32 @@ def train_model(net, criterion, optimizer, n_epochs):
             optimizer.step()
 
             pred = (outputs >= 0.5).int()
-
             accuracy = (labels == pred).float().mean() # corrected / total
 
             # print statistics
             running_loss += loss.item()
-            running_loss_bat += loss.item()
             running_correct += (labels==pred).sum().item()
-            if i % 200 == 199:    # print every 200 mini-batches
-                print('Epoch: %d, Batch: %5d, loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss_bat / 200))
-                print('Accuracy', accuracy.item())
-                minibatch_accuracies.append(accuracy.item())
-                running_loss_bat = 0.0
 
-        epoch_duration = time.time() - since
-        epoch_loss = running_loss / len(train_loader)
-        epoch_acc = 100 / 64 * running_correct / len(train_loader)
-        print("Epoch %s, duration: %d s, loss: %.4f, acc: %.4f" % (epoch+1, epoch_duration, epoch_loss, epoch_acc))
-            
-        losses.append(epoch_loss)
-        accuracies.append(epoch_acc)
+            if i % 200 == 199:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.6f' %(epoch + 1, i + 1, running_loss / 200))
+                print('Accuracy', accuracy.item())
+                losses.append(running_loss)
+                accuracies.append(accuracy.item())
+                running_loss = 0.0
+
+        # evaluate models
+        net.eval()
+        test_acc = model_eval(net)
+        test_accuracies.append(test_acc)
+
+        # reset to train
+        net.train()
+        #scheduler.step(test_acc)
 
     print('Finished Training')
-    return net, losses, accuracies, minibatch_accuracies
+    return net, losses, accuracies, test_accuracies
 
    
-def model_eval(net):
-    """ evaluate test set 
-    """
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for data in test_loader:
-            images, labels = data
-            outputs = net(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-    test_accuracy = 100 * correct / total
-    print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
-    return test_accuracy
 
 vocab_size = len(train_vocab)+2
 
@@ -224,8 +242,8 @@ class MyModel(nn.Module):
             nn.Conv2d(1, 3, 3, 1, 1),
             nn.ReLU())
 
-        self.fc11 = nn.Linear(19, 19*19)
-        self.fc12 = nn.Linear(19*19, 19)    
+        self.fc11 = nn.Linear(19*19, 19*19)
+        #self.fc12 = nn.Linear(19*19, 19)    
         
         self.tanh = nn.Tanh()
         self.relu = nn.ReLU()
@@ -234,7 +252,7 @@ class MyModel(nn.Module):
         self.fc2 = nn.Linear(64, 32)
         self.fc3 = nn.Linear(32, 1)
 
-        self.bil = nn.Bilinear(6862, 128, 64)
+        self.bil = nn.Bilinear(364, 128, 64)
 
         # layer for comments
         self.embedding = nn.Embedding(vocab_size, 100)
@@ -251,8 +269,9 @@ class MyModel(nn.Module):
         #x2 = x2.unsqueeze(3)
         #x1 = self.features1(x1)
         #x2 = self.features2(x2)
-
-        x1 = self.sp(self.fc11(x_board))
+        
+        x1 = x_board.view(-1, 19*19)
+        x1 = self.relu(self.fc11(x1))
         #x1 = self.sp(self.fc12(x1))
 
         text_emb = self.embedding(text)
@@ -272,7 +291,7 @@ class MyModel(nn.Module):
 
         x1 = x1.view(x1.size(0), -1)
         x2 = x_move.view(x_move.size(0), -1)
-        x3 = x3.view(x3.size(0), -1)
+        #x3 = x3.view(x3.size(0), -1)
         
         x = torch.cat((x1, x2), dim=1)
         x = self.bil(x, x3)
@@ -282,15 +301,16 @@ class MyModel(nn.Module):
   
         final_out = torch.sigmoid(self.fc3(x))
         return final_out
-   
 
 net_cnnlstm_test = MyModel()
 
 # set up criteria
 criterion = nn.BCELoss()
-optimizer = optim.Adam(net_cnnlstm_test.parameters())
+optimizer = optim.Adam(net_cnnlstm_test.parameters(), lr=0.001)
 
-model, losses, accuracies, acc_mb = train_model(net_cnnlstm_test, criterion, optimizer, n_epochs = 5)
+begin = time.time()
+model, losses, accuracies, t_accuracies = train_model(net_cnnlstm_test, criterion, optimizer, n_epochs = 50)
+total = time.time() - begin
 
 print(time.time)
 #output = net_cnnlstm_test(x1,x2,x3,x4)
